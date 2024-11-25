b5682b8d0a1781e9cde11249d3bd4a91bafb5e5d
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2024-11-25 20:47:55 +0300

    Completed user service unit tests

--- surveyhub/src/test/java/com/zekiyetekin/surveyhub/service/implementation/UserTest.java
+++ surveyhub/src/test/java/com/zekiyetekin/surveyhub/service/implementation/UserTest.java
@@ -1,19 +1,22 @@
 package com.zekiyetekin.surveyhub.service.implementation;
 
+import com.zekiyetekin.surveyhub.dto.UserDto;
 import com.zekiyetekin.surveyhub.entity.ResponseModel;
 import com.zekiyetekin.surveyhub.entity.User;
 import com.zekiyetekin.surveyhub.enumuration.responsemodel.ResponseMessageEnum;
 import com.zekiyetekin.surveyhub.enumuration.responsemodel.ResponseStatusEnum;
+import com.zekiyetekin.surveyhub.mapper.UserMapper;
 import com.zekiyetekin.surveyhub.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
@@ -23,10 +26,16 @@ class UserTest {
 
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.openMocks(this);
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn("encodedPassword");
     }
 
     @Test
@@ -67,7 +76,64 @@ class UserTest {
         assertEquals(ResponseMessageEnum.DATA_NOT_FOUND, response.getMessage());
         assertFalse(response.getSuccess());
         assertNull(response.getData());
-
     }
+
+    @Test
+    void testUpdate_Success() {
+        // Arrange
+        UserDto userDto = new UserDto();
+        userDto.setId(1);
+        userDto.setPassword("newPassword");
+        userDto.setAge(25);
+        userDto.setJob("Engineer");
+        userDto.setName("Test User");
+
+        User user = new User();
+        user.setId(1);
+        user.setPassword("encodedPassword");
+        user.setAge(25);
+        user.setJob("Engineer");
+        user.setName("Test User");
+
+        when(userRepository.findById(1)).thenReturn(Optional.of(user));
+        when(bCryptPasswordEncoder.encode("newPassword")).thenReturn("encodedPassword");
+        when(userMapper.toDto(user)).thenReturn(userDto);
+
+        // Act
+        ResponseModel<UserDto> response = userService.update(userDto);
+
+        // Assert
+        assertEquals(ResponseStatusEnum.OK.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.UPDATED_SUCCESSFULLY_DONE, response.getMessage());
+        assertTrue(response.getSuccess());
+        assertEquals("Test User", response.getData().getName());
+    }
+
+    @Test
+    void testUpdate_Exception() {
+        //Arrange
+        Integer userId = 2;
+
+        UserDto userDto = new UserDto();
+        userDto.setId(userId);
+        userDto.setPassword("newPassword");
+        userDto.setAge(22);
+        userDto.setJob("Engineer");
+        userDto.setName("Test User");
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        //Act:
+        ResponseModel<UserDto> response = userService.update(userDto);
+
+        // Assert
+        assertEquals(ResponseStatusEnum.NOT_FOUND.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.UPDATED_ERROR, response.getMessage());
+        assertFalse(response.getSuccess());
+        assertEquals(userDto,response.getData());
+    }
+
+
+
 
 }
