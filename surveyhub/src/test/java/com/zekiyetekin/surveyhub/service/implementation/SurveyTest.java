108520ef24436dc9216c891a0c638820a4e49f5e
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2024-12-13 21:50:36 +0300

    Completed unit testing of methods in SurveyService

--- surveyhub/src/test/java/com/zekiyetekin/surveyhub/service/implementation/SurveyTest.java
+++ surveyhub/src/test/java/com/zekiyetekin/surveyhub/service/implementation/SurveyTest.java
@@ -6,6 +6,7 @@ import com.zekiyetekin.surveyhub.entity.Survey;
 import com.zekiyetekin.surveyhub.entity.User;
 import com.zekiyetekin.surveyhub.enumuration.responsemodel.ResponseMessageEnum;
 import com.zekiyetekin.surveyhub.enumuration.responsemodel.ResponseStatusEnum;
+import com.zekiyetekin.surveyhub.filter.SurveyFilter;
 import com.zekiyetekin.surveyhub.mapper.SurveyMapper;
 import com.zekiyetekin.surveyhub.repository.SurveyRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +14,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDate;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -116,5 +119,131 @@ class SurveyTest {
         verify(surveyRepository, times(1)).findSurveysByUser_Id(userId);
         verify(surveyMapper,times(0)).convertList(any());
     }
+
+    @Test
+    void testSurveysById_Success(){
+        Integer surveyId = 1;
+        Survey survey = new Survey();
+        survey.setId(surveyId);
+
+        when(surveyRepository.findById(surveyId)).thenReturn(Optional.of(survey));
+        when(surveyRepository.save(survey)).thenReturn(survey);
+
+        ResponseModel<Survey> response = surveyService.getSurveysById(surveyId);
+
+        assertNotNull(response);
+        assertEquals(ResponseStatusEnum.OK.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.SUCCESSFULLY_DONE, response.getMessage());
+        assertTrue(response.getSuccess());
+        assertEquals(survey, response.getData());
+    }
+
+    @Test
+    void testSurveysById_Exception(){
+        Integer surveyId = 1;
+        when(surveyRepository.findById(surveyId)).thenReturn(Optional.empty());
+
+        ResponseModel<Survey> response = surveyService.getSurveysById(surveyId);
+
+        assertNotNull(response);
+        assertEquals(ResponseStatusEnum.NOT_FOUND.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.DATA_NOT_FOUND, response.getMessage());
+        assertFalse(response.getSuccess());
+        assertNull(response.getData());
+    }
+
+    @Test
+    void testSearchByDateWithFilter_Success(){
+        SurveyFilter surveyFilter = new SurveyFilter();
+        surveyFilter.setCreatedAt(LocalDate.of(2023, 5, 1));
+
+        Survey survey = new Survey();
+        survey.setId(1);
+        survey.setCreatedAt(LocalDate.of(2023, 5, 1));
+
+        List<Survey> surveyList = List.of(survey);
+
+        List<SurveyDto> surveyDtoList = List.of(new SurveyDto());
+
+        when(surveyRepository.findAll(any(Specification.class))).thenReturn(surveyList);
+        when(surveyMapper.convertList(surveyList)).thenReturn(surveyDtoList);
+
+        ResponseModel<List<SurveyDto>> response = surveyService.searchByDateWithFilter(surveyFilter);
+
+        assertEquals(ResponseStatusEnum.OK.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.SEARCHED_SUCCESSFULLY, response.getMessage());
+        assertTrue(response.getSuccess());
+        assertNotNull(response.getData());
+        assertEquals(1, response.getData().size());
+
+        verify(surveyRepository, times(1)).findAll(any(Specification.class));
+        verify(surveyMapper, times(1)).convertList(surveyList);
+    }
+
+    @Test
+    void testSearchByDateWithFilter_Exception(){
+        SurveyFilter surveyFilter = new SurveyFilter();
+        surveyFilter.setCreatedAt(LocalDate.of(2024,6,21));
+
+        when(surveyRepository.findAll(any(Specification.class))).thenReturn(new ArrayList<>());
+        ResponseModel<List<SurveyDto>> response = surveyService.searchByDateWithFilter(surveyFilter);
+
+        // Assert
+        assertEquals(ResponseStatusEnum.NOT_FOUND.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.SEARCHED_ERROR, response.getMessage());
+        assertFalse(response.getSuccess());
+        assertNull(response.getData());
+
+        // Verify
+        verify(surveyRepository, times(1)).findAll(any(Specification.class));
+        verify(surveyMapper, times(0)).convertList(any());
+    }
+
+    @Test
+    void testSearchByCategoryWithFilter_Success(){
+        SurveyFilter surveyFilter = new SurveyFilter();
+        surveyFilter.setCategory("Teknology");
+
+        Survey survey = new Survey();
+        survey.setId(1);
+        survey.setCategory("Teknology");
+
+        List<Survey> surveyList = List.of(survey);
+        List<SurveyDto> surveyDtoList = List.of(new SurveyDto());
+
+        when(surveyRepository.findAll(any(Specification.class))).thenReturn(surveyList);
+        when(surveyMapper.convertList(surveyList)).thenReturn(surveyDtoList);
+
+        ResponseModel<List<SurveyDto>> response = surveyService.searchByCategoryWithFilter(surveyFilter);
+
+        assertEquals(ResponseStatusEnum.OK.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.SEARCHED_SUCCESSFULLY, response.getMessage());
+        assertTrue(response.getSuccess());
+        assertNotNull(response.getData());
+        assertEquals(1, response.getData().size());
+
+        verify(surveyRepository, times(1)).findAll(any(Specification.class));
+        verify(surveyMapper, times(1)).convertList(surveyList);
+    }
+
+    @Test
+    void testSearchByCategoryWithFilter_Exception(){
+        SurveyFilter surveyFilter = new SurveyFilter();
+        surveyFilter.setCategory("NonExistingCategory");
+
+        when(surveyRepository.findAll(any(Specification.class))).thenReturn(new ArrayList<>());
+        ResponseModel<List<SurveyDto>> response = surveyService.searchByDateWithFilter(surveyFilter);
+
+        // Assert
+        assertEquals(ResponseStatusEnum.NOT_FOUND.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.SEARCHED_ERROR, response.getMessage());
+        assertFalse(response.getSuccess());
+        assertNull(response.getData());
+
+        // Verify
+        verify(surveyRepository, times(1)).findAll(any(Specification.class));
+        verify(surveyMapper, times(0)).convertList(any());
+    }
+
 
 }
