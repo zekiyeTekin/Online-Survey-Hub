e5724da1107ef97f40d594cec501ede34dd03801
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2024-12-17 00:01:47 +0300

    Created unit test for surveyUserService

--- surveyhub/src/test/java/com/zekiyetekin/surveyhub/service/implementation/SurveyUserTest.java
+++ surveyhub/src/test/java/com/zekiyetekin/surveyhub/service/implementation/SurveyUserTest.java
@@ -1,5 +1,6 @@
 package com.zekiyetekin.surveyhub.service.implementation;
 
+import com.zekiyetekin.surveyhub.dto.SurveyDto;
 import com.zekiyetekin.surveyhub.dto.SurveyUserDto;
 import com.zekiyetekin.surveyhub.entity.ResponseModel;
 import com.zekiyetekin.surveyhub.entity.Survey;
@@ -7,8 +8,10 @@ import com.zekiyetekin.surveyhub.entity.SurveyUser;
 import com.zekiyetekin.surveyhub.entity.User;
 import com.zekiyetekin.surveyhub.enumuration.responsemodel.ResponseMessageEnum;
 import com.zekiyetekin.surveyhub.enumuration.responsemodel.ResponseStatusEnum;
+import com.zekiyetekin.surveyhub.mapper.SurveyMapper;
 import com.zekiyetekin.surveyhub.mapper.SurveyUserMapper;
 import com.zekiyetekin.surveyhub.repository.SurveyUserRepository;
+import com.zekiyetekin.surveyhub.service.SurveyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -29,6 +32,9 @@ public class SurveyUserTest {
     private SurveyUserRepository surveyUserRepository;
     @Mock
     private SurveyUserMapper surveyUserMapper;
+    @Mock
+    private SurveyMapper surveyMapper;
+
 
     @BeforeEach
     public void setUp(){
@@ -88,7 +94,39 @@ public class SurveyUserTest {
 
         verify(surveyUserRepository, times(1)).findSurveyUsersByUser(user);
         verify(surveyUserMapper, times(1)).convertList(surveyUserList);
+    }
 
+    @Test
+    void testGetNotParticipatedSurveys_Success(){
+        User user = new User();
+        user.setId(1);
+
+        List<Survey> surveyList = new ArrayList<>();
+        Survey survey = new Survey();
+        survey.setId(1);
+        survey.setName("Test survey");
+        surveyList.add(survey);
+
+        List<SurveyDto> surveyDtoList = new ArrayList<>();
+        SurveyDto surveyDto = new SurveyDto();
+        surveyDto.setId(1);
+        surveyDto.setName("Test survey");
+        surveyDtoList.add(surveyDto);
+
+        when(surveyUserRepository.findSurveysNotParticipatedByUser(user)).thenReturn(surveyList);
+        when(surveyMapper.convertList(surveyList)).thenReturn(surveyDtoList);
+
+        ResponseModel<List<SurveyDto>> response = surveyUserService.getNotParticipatedSurveys(user);
+
+        assertEquals(ResponseStatusEnum.OK.getCode(), response.getCode());
+        assertEquals(ResponseMessageEnum.LISTING_SUCCESSFULLY_DONE, response.getMessage());
+        assertTrue(response.getSuccess());
+        assertNotNull(response.getData());
+        assertEquals(1, response.getData().size());
+        assertEquals(1, response.getData().get(0).getId());
+
+        verify(surveyUserRepository, times(1)).findSurveysNotParticipatedByUser(user);
+        verify(surveyMapper, times(1)).convertList(surveyList);
 
     }
 
